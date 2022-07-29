198c669ddd2da2662efbbe3206c392e430c6d6c4
rabosian <msann1356@gmail.com>
2022-07-29 13:16:45 -0400

    finish navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import UserIcon from "@mui/icons-material/PersonOutline";
+import LogoIcon from "@mui/icons-material/Checkroom";
+import SearchIcon from "@mui/icons-material/Search";
+import { IconButton, TextField } from "@mui/material";
+import { Navigate } from 'react-router-dom'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const menuList = [
+    "Women",
+    "Men",
+    "Baby",
+    "Kids",
+    "Home",
+    "Sale",
+    "Sustainability",
+  ];
 
-export default Navbar
+  return (
+    <div>
+      <div className="login-btn">
+        <IconButton>
+          <UserIcon />
+          LOGIN
+        </IconButton>
+      </div>
+      <div className="logo">
+          <LogoIcon fontSize="large" />
+      </div>
+      <div className="menu-section">
+        <ul className="menu-list">
+          {menuList.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+
+        <div className="search-bar">
+          <SearchIcon fontSize="small" />
+          <input type="text" placeholder="search for" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
