70a7b5136e4e1caaf303af9f8cafe63d45c54832
rabosian <msann1356@gmail.com>
2022-07-29 12:21:06 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+// Task List
+// Pages: Products, Login, Detail
+// user ? <Detail /> : redirect to <Login />
+// Click login/logout toggles user login
+// login button toggles text=> user ? Logout : Login
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
