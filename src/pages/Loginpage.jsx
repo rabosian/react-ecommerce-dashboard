5919535bc95d28973a63d1a793498d9eb14e46ea
rabosian <msann1356@gmail.com>
2022-07-30 23:58:32 -0400

    edit Loginpage

--- src/pages/Loginpage.jsx
+++ src/pages/Loginpage.jsx
@@ -1,8 +1,41 @@
 import React from 'react'
-
+import { Grid, Paper, Avatar, TextField, Button } from '@mui/material'
 const Loginpage = () => {
   return (
-    <div>Loginpage</div>
+    <div><Navbar />
+    <Grid align="center">
+      <Paper elevation={20}>
+        <Grid align="center">
+          <Avatar></Avatar>
+          <h2>Log In</h2>
+        </Grid>
+          <TextField
+            required
+            variant="standard"
+            type="email"
+            fullWidth
+            label="Email"
+          />
+          <TextField
+            required
+            variant="standard"
+            type="password"
+            fullWidth
+            label="Password"
+          />
+          {/* <FormControlLabel
+            control={<Checkbox name="checkedA" />}
+            label="Remember me"
+          /> */}
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+          >
+            Log In
+          </Button>
+      </Paper>
+    </Grid></div>
   )
 }
 
